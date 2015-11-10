e1cc0514e27e0126f07af43c56ba1374346ba7f3
Laurent Duchesne <l@urent.org>
2015-11-10 16:16:26 -0500

    Added copy constructors for base types.

--- types.h
+++ types.h
@@ -20,7 +20,11 @@ class geom_container
 {
   public:
       geom_container() {}
-      geom_container(const geom_container& other): geom(GEOSGeom_clone(other.geom)) {}
+      geom_container(const geom_container& other, bool clone = true): geom(other.geom) {
+          if (clone) {
+              geom = GEOSGeom_clone(other.geom);
+          }
+      }
 
       virtual ~geom_container();
 
@@ -40,10 +44,10 @@ class edge : public geom_container {
   public:
     edge(): geom_container() {}
 
-    edge(const edge* other): edge(*other) {}
+    edge(const edge* other, bool clone_geom = true): edge(*other, clone_geom) {}
 
-    edge(const edge& other):
-        geom_container(other),
+    edge(const edge& other, bool clone_geom = true):
+        geom_container(other, clone_geom),
         id(other.id),
         start_node(other.start_node),
         end_node(other.end_node),
@@ -73,15 +77,32 @@ class edge : public geom_container {
 
 class node : public geom_container {
   public:
+    node(): geom_container() {}
+
+    node(const node* other, bool clone_geom = true): node(*other, clone_geom) {}
+
+    node(const node& other, bool clone_geom = true):
+        geom_container(other, clone_geom),
+        id(other.id),
+        containing_face(other.containing_face) {};
+
     virtual ~node() {}
     virtual bool intersects(const GEOSGeometry* geom);
-    
+
     int id = NULLint;
     int containing_face = NULLint;
 };
 
 class face : public geom_container {
   public:
+    face(): geom_container() {}
+
+    face(const face* other, bool clone_geom = true): face(*other, clone_geom) {}
+
+    face(const face& other, bool clone_geom = true):
+        geom_container(other, clone_geom),
+        id(other.id) {};
+
     virtual ~face() {}
 
     int id = NULLint;
