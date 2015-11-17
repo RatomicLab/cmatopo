1cd916b67c94d43ba46ab8ccf82c338ed460dc95
Laurent Duchesne <l@urent.org>
2015-11-17 09:57:05 -0500

    Added GEOS helper to print a geometry.

--- utils.cpp
+++ utils.cpp
@@ -3,6 +3,9 @@
 #include <omp.h>
 #include <cstdio>
 #include <stdarg.h>
+#include <iostream>
+
+using namespace std;
 
 namespace cma {
 
@@ -20,6 +23,11 @@ void geos_message_function(const char *fmt, ...)
     va_start(ap, fmt);
     printf(fmt, ap);
     va_end (ap);
+}
+
+void GEOSHelper::print_geom(const GEOSGeometry* geom)
+{
+    cout << as_string(geom) << endl;
 }
 
 } // namespace cma
